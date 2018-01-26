6e9f413055b24bef72a99ebac1d00909b02a92f2
BloodyLink <david.quezada.m@gmail.com>
2018-01-26 16:50:33 -0300

    pokeApi
    trying out PokeApi

---
+++ apiRequest.js
@@ -0,0 +1,32 @@
+var Request = require("request");
+
+// Request.get("http://pokeapi.co/api/v2/pokemon/1/", (error, response, body) => {
+//     if(error) {
+//         return console.dir(error);
+//     }
+//     console.dir(JSON.parse(body));
+// });
+
+var Pokedex = require('pokedex-promise-v2');
+var P = new Pokedex();
+
+P.getPokemonByName('pikachu') // with Promise
+.then(function(response) {
+  console.log(response);
+})
+.catch(function(error) {
+  console.log('There was an ERROR: ', error);
+});
+
+// P.getPokemonByName(34, function(response, error) { // with callback
+//   if(!error) {
+//     console.log(response);
+//   } else {
+//     console.log(error)
+//   }
+// });
+
+// P.resource(['/api/v2/pokemon/36', 'api/v2/berry/8', 'https://pokeapi.co/api/v2/ability/9/'])
+// .then(function(response) {
+//   console.log(response); // resource function accepts singles or arrays of URLs/paths
+// });
