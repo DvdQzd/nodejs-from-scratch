17b96e1a60d57a1b17a8149ada3b568c98f5129b
BloodyLink <david.quezada.m@gmail.com>
2018-01-29 20:01:51 -0300

    testing

--- apiRequest.js
+++ apiRequest.js
@@ -1,4 +1,5 @@
 var Request = require("request");
+var http = require("http");
 
 // Request.get("http://pokeapi.co/api/v2/pokemon/1/", (error, response, body) => {
 //     if(error) {
@@ -10,13 +11,51 @@ var Request = require("request");
 var Pokedex = require('pokedex-promise-v2');
 var P = new Pokedex();
 
-P.getPokemonByName('corsola') // with Promise
+var getPokemon = function(name){
+  P.getPokemonByName(name) // with Promise
 .then(function(response) {
-  console.log(response);
+   console.log(response);
+  //return response.name;
 })
 .catch(function(error) {
   console.log('There was an ERROR: ', error);
 });
+}
+
+// getPokemon('lugia');
+
+
+var handler = function(request,response){
+  console.log("Request received");
+  response.setHeader('Access-Control-Allow-Origin', 'http://localhost');
+  response.writeHeader(200, {"Content-Type": "application/json"}); 
+
+  var pokemon = "";
+
+  request.on('data', function (chunk) {
+      pokeName = chunk.toString('utf8');
+      console.log("Data of " + pokeName);
+                  // console.log(JSON.stringify(getPokemon('lugia')));
+      //getPokemon(chunk.toString('utf8'));
+      //  response.write(getPokemon(pokeName));
+      //console.log(JSON.stringify(getPokemon(pokeName)));
+
+      pokemon = getPokemon(pokeName);
+
+      //  response.end(getPokemon(pokeName));
+  });     
+
+  response.on('end', () => {
+    console.log(JSON.parse(getPokemon(pokeName)));
+  });
+
+  //response.end(pokemon);
+  //response.write("oji");
+};
+
+var server = http.createServer(handler);
+
+server.listen(8080);
 
 
 // P.resource(['/api/v2/pokemon/36', 'api/v2/berry/8', 'https://pokeapi.co/api/v2/ability/25/'])
